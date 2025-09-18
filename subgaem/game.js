2ccc6166d1d5a7a782698d8d1346256c6fa19669
Javier <jhonny.fl2006@gmail.com>
2025-09-18 00:59:54 -0600

    Nuevo inicio de proyecto

---
+++ subgaem/game.js
@@ -0,0 +1,18 @@
+//board
+let board;
+const rowCount = 21;
+const colCount = 19;
+const tileSize = 32;
+const boardWidth = colCount * tileSize;
+const boardHeight = rowCount * tileSize;
+let constext;
+
+//imagenes
+
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //dibujo del tablero
+}
