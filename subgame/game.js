f5a605a1a5ee0174c74d2eec1316e3df8879d2fc
Javier <Jhonny.fl2006@gmail.com>
2025-10-05 15:45:44 -0600

    hola

--- subgame/game.js
+++ subgame/game.js
@@ -8,11 +8,41 @@ const boardHeight = rowCount * tileSize;
 let constext;
 
 //imagenes
-
+let momia1Image;
+let momia2Image;
+let quest1Image;
+let quest2Image;
+let quest_muerto;
+let wallImage
 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d"); //dibujo del tablero
+
+    loadImages();
+}
+
+function loadImages () {
+    wallImage = new Image ();
+    wallImage.src = "" //imagen de caja
+
+    momia1Image = new Image ();
+    momia1Image.src = "" //imagen de momia1
+
+    momia2Image = new Image ();
+    momia2Image.src = "" //imagen de momia2
+
+    quest1Image = new Image ();
+    quest1Image.src = "" //imagen de quest 1
+
+    quest2Image = new Image ();
+    quest2Image.src = "" //imagen de quest 2
+
+    quest_muerto = new Image ();
+    quest_muerto.src = "" //imagen de questmuerto
+
+
+
 }
